e1c0a1e93e4f01961ef9f623f3da6e0883b44951
Biplab Dutta <biplabdutta860@gmail.com>
2023-01-09 00:38:58 +0545

    edit entry points

--- demo_custom_lint/bin/custom_lint.dart
+++
@@ -1,3 +0,0 @@
-import 'package:custom_lint_builder/custom_lint_builder.dart';
-
-class DemoCustomLint extends PluginBase {}
---
+++ demo_custom_lint/lib/demo_custom_lint.dart
@@ -0,0 +1,5 @@
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+
+DemoCustomLint createPlugin() => DemoCustomLint();
+
+class DemoCustomLint extends PuginBase {}
